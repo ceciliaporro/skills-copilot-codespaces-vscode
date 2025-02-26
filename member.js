560246fd3a8100c49e7b4909c28576dae75481e7
Cecilia Porro <80780162+ceciliaporro@users.noreply.github.com>
2025-02-26 09:26:32 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+    return {
+        "skills": [
+            {
+                "name": "HTML",
+                "level": 10
+            },
+            {
+                "name": "CSS",
+                "level": 9
+            }
+        ]
+    }
+}
